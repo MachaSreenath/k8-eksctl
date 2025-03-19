307ec65e43686bd57aa823a6c55945df6b9c2bd8
MachaSreenath <sreenathshree9@gmail.com>
2025-03-19 13:04:37 +0530

    kube eksctl changes

--- workspace.sh
+++ workspace.sh
@@ -73,3 +73,33 @@ tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
 
 VALIDATE $? "eksctl installation"
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+
+chmod 700 get_helm.sh
+
+./get_helm.sh
+
+VALIDATE $? "helm installation"
+
+curl -sS https://webinstall.dev/k9s | bash
+
+VALIDATE $? "k9 installation"
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+VALIDATE $? "kubens installation"
+
+helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
+
+helm repo update
+
+helm upgrade --install aws-ebs-csi-driver --namespace kube-system aws-ebs-csi-driver/aws-ebs-csi-driver
+
+VALIDATE $? "ebs drivers installation"
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+VALIDATE $? "Metrics server installation"
